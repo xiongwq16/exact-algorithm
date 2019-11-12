b794cbd21a410491ce2d424e2ef4f145998be2b0
xiongwq <857930804@qq.com>
2019-11-12 14:49:57 +0800

    try to deal with branch node having no initial solution to start CG

--- src/vrptw/algorithm/solomoninsertion/SolomonInsertion.java
+++ src/vrptw/algorithm/solomoninsertion/SolomonInsertion.java
@@ -16,11 +16,15 @@ import vrptw.solution.Path;
  * @version V1.0
  * @since JDK1.8
  */
-public class SolomonInsertion {
-    private Vrptw vrptwIns;
+public class SolomonInsertion {    
+    /** 时间矩阵，分支过程中 timeMatrix 会更新，并且需要调用 Solomon Insertion 生成初始解. */
+    static double[][] timeMatrix;
+    static Vrptw vrptwIns;
+    
+    private int cusNum;
+    private int vertexNum;
     
     private int routeNum;
-    
     private ArrayList<Route> routes;
     
     /** is the vertex visited. */
@@ -31,65 +35,82 @@ public class SolomonInsertion {
      * 
      * @param vrptwIns VRPTW instance
      */
-    public SolomonInsertion(Vrptw vrptwIns) {
-        this.vrptwIns = vrptwIns;
+    public SolomonInsertion(Vrptw vrptwIns, double[][] timeMatrix) {
+        SolomonInsertion.timeMatrix = timeMatrix;
+        SolomonInsertion.vrptwIns = vrptwIns;
+        
+        cusNum = vrptwIns.getCusNum();
+        vertexNum = vrptwIns.getVertexNum();
         
         routes = new ArrayList<>(Parameters.INITIAL_CAPACITY);
         
         // false default，为了简化编程，把 depot 也放进来了且默认为 true
         isVisited = new boolean[vrptwIns.getVertexNum()];  
         isVisited[0] = true;
-        isVisited[vrptwIns.getVertexNum() - 1] = true;
+        isVisited[vertexNum - 1] = true;
     }
     
     /**
      * 用I1算法构造路径.
      */
     public void constructRoutes() {
-        // 初始化 route 的静态变量
-        Route.vrptwIns = vrptwIns;
+        // TODO 对于 Branch and Bound 中分支节点无初始可行解的尝试，需要下一阶段的优化
+        // 为不能第一个被访问、不能最后一个被访问的客户生成路径，可能存在同一个客户在多条路径上的情况
+        for (int i = 1; i < vertexNum - 1; i++) {
+            Route r;
+            if (timeMatrix[0][i] == Double.MAX_VALUE) {
+                if (timeMatrix[i][vertexNum - 1] == Double.MAX_VALUE) {
+                    // 不能第一个被访问，也不能最后一个访问，找一条形如 startDepot-v1-i-v2-endDepot 的可行路径
+                    r = this.generateRouteForMiddleCus(i);
+                } else {
+                    // 不能第一个被访问，找一条形如 startDepot-v1-i-endDepot 的可行路径
+                    r = this.generateRouteWithForLastCus(i);
+                }
+                
+                if (r == null) {
+                    // 无解
+                    return;
+                }
+                
+                this.addRoute(r);
+                continue;
+            }
+            
+            if (timeMatrix[i][vertexNum - 1] == Double.MAX_VALUE) {
+                // 不能最后一个被访问，找一条形如 startDepot-v1-i-endDepot 的可行路径
+                r = this.generateRouteWithForFirstCus(i);
+                
+                if (r == null) {
+                    // 无解
+                    return;
+                }
+                this.addRoute(r);              
+            }
+            
+        }
         
-        // select a seed customer randomly
+        int visitedCusNum = 0;
+        visitedCusNum = this.getVisitedCusNum();
         Random rand = new Random(Parameters.RANDOM_SEED);
         
-        // 生成 [1, vertexNum - 1] 内的随机整数，对应客户 ID
-        int seedCusId = 1 + rand.nextInt(vrptwIns.getVertexNum() - 1);
-        this.createRouteForSeedCus(seedCusId);
+        // 准备构造一条新的路径，在剩余未被服务的客户中随机选择一个客户
+        int randInt = 1 + rand.nextInt(cusNum - visitedCusNum);
+        this.createRouteForSeedCus(randInt);
         
         // 核心步骤，依次构造每一条路径
         while (true) {
-            int cusNum = vrptwIns.getCusNum();
             // 判断是否所有的客户都已分配
-            int servedVertexNum = 0;
-            for (int i = 1; i <= cusNum; i++) {
-                if (isVisited[i] == true) {
-                    servedVertexNum++;
-                }
-            }
-            if (servedVertexNum == cusNum) {
+            visitedCusNum = this.getVisitedCusNum();
+            if (visitedCusNum == cusNum) {
                 break;
             }
-                        
+            
             // 得到最优插入顾客和插入位置
             BestInsert bestInsert = this.getBestInsertCusInfo();
             if (bestInsert.bestCusId == -1 || bestInsert.bestInsertPos == -1) {
                 // 准备构造一条新的路径，在剩余未被服务的客户中随机选择一个客户
-                int randInt = 1 + rand.nextInt(cusNum - servedVertexNum);
-                int count = 0;
-                for (int id = 0; id <= cusNum; id++) {
-                    if (isVisited[id]) {
-                        continue;
-                    }
-                    
-                    // 找到第 randInt 个未被服务的客户
-                    count++;
-                    if (count == randInt) {
-                        seedCusId = id;
-                        break;
-                    }
-                    
-                }
-                this.createRouteForSeedCus(seedCusId);
+                randInt = 1 + rand.nextInt(cusNum - visitedCusNum);
+                this.createRouteForSeedCus(randInt);
                 
             } else {
                 Route r = routes.get(routeNum - 1);
@@ -117,6 +138,10 @@ public class SolomonInsertion {
         return paths;
     }
     
+    public double getCost() {
+        return routes.stream().mapToDouble(route -> route.getCost()).sum(); 
+    }
+    
     /**
      * 输出 Solomon Insertion Algorithm 生成的初始解.
      */
@@ -134,21 +159,29 @@ public class SolomonInsertion {
             }
         }
         
-        double cusNumServed = 0;
         for (Route r: routes) {
-            cusNumServed += r.getCusNum();
             sb.append(r.toString() + "\n");
-        }
-        
-        if (cusNumServed != vrptwIns.getCusNum()) {
-            System.out.println("Infesible");
-            return;
         }
         
         System.out.println(sb.toString());
     }
     
-    private void createRouteForSeedCus(int seedCusId) {
+    private void createRouteForSeedCus(int randInt) {        
+        int count = 0;
+        int seedCusId = 1;
+        for (int id = 1; id < vertexNum - 1; id++) {
+            if (isVisited[id]) {
+                continue;
+            }
+            
+            // 找到第 randInt 个未被服务的客户
+            count++;
+            if (count == randInt) {
+                seedCusId = id;
+                break;
+            }
+        }
+        
         // add seed customer
         Route r = new Route();
         // add seed customer
@@ -157,6 +190,17 @@ public class SolomonInsertion {
         
         routes.add(r);
         routeNum++;
+    }
+    
+    private int getVisitedCusNum() {
+        int servedVertexNum = 0;
+        for (int i = 1; i <= cusNum; i++) {
+            if (isVisited[i] == true) {
+                servedVertexNum++;
+            }
+        }
+        
+        return servedVertexNum;
     }
     
     /**
@@ -172,7 +216,7 @@ public class SolomonInsertion {
         double maxSaving = -100000000;
         
         // 0 为 start depot，vertexNum - 1 为 end depot(dummy)
-        for (int cusId = 0; cusId < vrptwIns.getVertexNum() - 1; cusId++) {
+        for (int cusId = 0; cusId < vertexNum - 1; cusId++) {
             if (isVisited[cusId]) {
                 continue;
             }
@@ -242,7 +286,7 @@ public class SolomonInsertion {
         if (pos == r.getCusNum()) {
             // 插入到最后一个顾客和depot之间
             preVertexId = r.getVertexIds().get(r.getCusNum());
-            nextVertexId = vrptwIns.getVertexNum() - 1;
+            nextVertexId = vertexNum - 1;
         } else {
             preVertexId = r.getVertexIds().get(pos);
             nextVertexId = r.getVertexIds().get(pos + 1);
@@ -309,5 +353,92 @@ public class SolomonInsertion {
             this.bestCusId = bestCusId;
         }
         
-    }    
+    }
+    
+    /**
+     * 暴力搜索生成形如 startDepot-v1-cusId-v2-endDepot 的可行路径.
+     * 
+     * @param cusId 给定的客户 ID
+     * @return 生成的路径，如果不存在这样的路径，返回 null
+     */
+    private Route generateRouteForMiddleCus(int cusId) {
+        Route r = new Route();
+        // startDepot-cusId
+        r.addVertexToEnd(cusId);
+        
+        for (int i = 1; i < vertexNum - 1; i++) {
+            // startDepot-v1-cusId
+            r.insertCustomer(0, i);
+            
+            for (int j = 1; j < vertexNum - 1; j++) {
+                if (i == j) {
+                    continue;
+                }
+                
+                // startDepot-v1-cusId-v2
+                r.addVertexToEnd(j);
+                if (r.isFeasible()) {
+                    return r;
+                }
+                
+                r.removeCustomer(r.getCusNum());
+            }
+            r.removeCustomer(1);
+        }
+        
+        return null;
+    }
+    
+    /**
+     * 暴力搜索生成形如 startDepot-v1-cusId-endDepot 的可行路径.
+     * 
+     * @param cusId 给定的客户 ID
+     * @return 生成的路径，如果不存在这样的路径，返回 null
+     */
+    private Route generateRouteWithForLastCus(int cusId) {
+        Route r = new Route();
+        // startDepot-cusId
+        r.addVertexToEnd(cusId);
+        
+        for (int i = 1; i < vertexNum - 1; i++) {
+            // startDepot-v1-cusId
+            r.insertCustomer(0, i);
+            if (r.isFeasible()) {
+                return r;
+            }
+            r.removeCustomer(1);
+        }
+        
+        return null;
+    }
+    
+    /**
+     * 暴力搜索生成形如 startDepot-cusId-v1-endDepot 的可行路径.
+     * 
+     * @param cusId 给定的客户 ID
+     * @return 生成的路径，如果不存在这样的路径，返回 null
+     */
+    private Route generateRouteWithForFirstCus(int cusId) {
+        Route r = new Route();
+        // startDepot-cusId
+        r.addVertexToEnd(cusId);
+        
+        for (int i = 1; i < vertexNum - 1; i++) {
+            // startDepot-v1-cusId
+            r.addVertexToEnd(i);
+            if (r.isFeasible()) {
+                return r;
+            }
+            r.removeCustomer(1);
+        }
+        
+        return null;
+    }
+    
+    private void addRoute(Route r) {
+        routes.add(r);
+        for (int i = 1; i <= r.getCusNum(); i++) {
+            isVisited[i] = true;
+        }
+    }
 }
