10c78b4c7c2f01f333ab224309a9584dcd90809e
xiongwq <857930804@qq.com>
2019-12-03 12:30:29 +0800

    delete pachage-info from git rep

--- src/csp/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 基于Cplex的木材切割问题的列生成实现.
- * 
- * @author Xiong Wangqi
- * @version V1.0
- * @since JDK1.8
- * 2019-09-21 23:34:57
- */
-package csp;
--- src/uflp/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 基于 Cplex Benders Annotation 的 UFLP 的 Benders decomposition algorithm 实现.
- * 
- * @author Xiong Wangqi
- * @version V1.0
- * @since JDK1.8
- * 2019-09-21 23:39:55
- */
-package uflp;
